38b473ba486bd8bc7834701d529b5b2f7105cc00
printf-jin <dlghwls7662@naver.com>
2022-05-10 00:12:15 +0900

    html파일명 변경 변경전: index.html 변경후: login.html

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route("/homework", methods=["POST"])
 def homework_post():
