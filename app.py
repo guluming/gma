7fa06649c0025033b76525d74a4e3a7160ecee75
printf-jin <dlghwls7662@naver.com>
2022-05-11 11:37:51 +0900

    로그인 페이지 서버

--- app.py
+++ app.py
@@ -1,30 +1,70 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.ksf61.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.abw6w.mongodb.net/Cluster0?retryWrites=true&w=majority')
+db = client.dbsparta_plus_week4
+
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-    doc = {
-        'name': name_receive,
-        'comment': comment_receive,
-    }
-    db.homework.insert_one(doc)
+        return render_template('login.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-    return jsonify({'msg':'응원 남기기 성공!!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    comment_list = list(db.homework.find({}, {'_id': False}))
-    return jsonify({'comments':comment_list})
+@app.route('/login')
+def login():
+    # 로그인 화면으로 이동 기능
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/membership')
+def sign_up():
+    # 회원가입 화면으로 이동 기능
+    msg = request.args.get("msg")
+    return render_template('membership.html', msg=msg)
+
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인 기능
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
