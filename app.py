c9b575a2ff81eb96246a2da75ffb204c1bb921bd
guluming <guluming@naver.com>
2022-05-11 15:28:22 +0900

    DB 경로 변경

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 SECRET_KEY = 'SPARTA'
 
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ksf61.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta
+db = client.dbgma
 
 
 @app.route('/')
